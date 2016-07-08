f27e16892b48d0aaf40b4071e5e97c1698a6c8a1
Anshul Khanna <anshul17khanna@gmail.com>
2016-07-08 17:43:06 +0530

    integrate Google Now library

--- app/src/main/java/com/github/anshul17khanna/textspeaker/MainActivity.java
+++ app/src/main/java/com/github/anshul17khanna/textspeaker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.github.anshul17khanna.textspeaker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +12,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.zagum.speechrecognitionview.RecognitionProgressView;
+import com.github.zagum.speechrecognitionview.adapters.RecognitionListenerAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context = this;
+    SpeechRecognizer speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,44 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecognitionProgressView recognitionProgressView = (RecognitionProgressView) findViewById(R.id.recognition_view);
+        recognitionProgressView.setSpeechRecognizer(speechRecognizer);
+        recognitionProgressView.setRecognitionListener(new RecognitionListenerAdapter() {
+            @Override
+            public void onResults(Bundle results) {
+                showResults(results);
+            }
+        });
+
+        recognitionProgressView.play();
+
+        Button listen = (Button) findViewById(R.id.listen);
+        Button reset = (Button) findViewById(R.id.reset);
+
+        listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecognition();
+            }
+        });
+    }
+
+
+
+    private void startRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizer.startListening(intent);
+    }
+
+    private void showResults(Bundle results) {
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        // Toast.makeText(this, matches.get(0), Toast.LENGTH_LONG).show();
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setText(matches.get(0));
     }
 
     @Override
