2325ddc1b351b6f88bcf8cd57f1d431db7ac1a2e
Anshul Khanna <anshul17khanna@gmail.com>
2016-07-09 00:59:14 +0530

    STT done

--- app/src/main/java/com/github/anshul17khanna/textspeaker/MainActivity.java
+++ app/src/main/java/com/github/anshul17khanna/textspeaker/MainActivity.java
@@ -33,15 +33,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         final RecognitionProgressView recognitionProgressView = (RecognitionProgressView) findViewById(R.id.recognition_view);
         recognitionProgressView.setSpeechRecognizer(speechRecognizer);
         recognitionProgressView.setRecognitionListener(new RecognitionListenerAdapter() {
